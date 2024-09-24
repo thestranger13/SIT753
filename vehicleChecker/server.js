5ea25a07e81b3406b66a6abc760b19a43db9c920
Camille Ang <S223646635@deakin.edu.au>
2024-09-24 20:43:15 +1000

    Add Release Stage Documents

--- vehicleChecker/server.js
+++ vehicleChecker/server.js
@@ -9,25 +9,25 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Server will be on port 3000, or a different port for testing
+// server will be on port 3000 
 const port = process.env.PORT || 3000;
 
-// Mapped to vehicleRoutes for any incoming requests
+// mapped to vehicleRoutes for any incoming requests
 app.use('/checker', vehicleRoutes);
 
 // Create HTTP server from Express app
 const server = http.createServer(app);
 
-// Setup the websocket server
+// setup the websocket server
 const wss = new WebSocket.Server({ server });
 
-// Count of clients and vehicle data count
+// the count of clients will always start from 0 and vehicle data count will be 3
 let connectedClients = 0;
 let vehicleDataCount = 3;
 
-// Function to broadcast data to all clients
+// function to broadcast data to all clients
 function broadcastData() {
-    const randomIncrement = Math.floor(Math.random() * 5) + 1; // Add a random number from 1 to 5 to the vehicleDataCount
+    const randomIncrement = Math.floor(Math.random() * 5) + 1; // add a random number from 1 to 5 to the vehicleDataCount
     vehicleDataCount += randomIncrement;
 
     wss.clients.forEach(client => {
@@ -40,7 +40,7 @@ function broadcastData() {
     });
 }
 
-// WebSocket connection handler
+// websocket connection handler
 wss.on('connection', (ws) => {
     connectedClients++;
     console.log('New client connected. Total clients:', connectedClients);
@@ -51,22 +51,18 @@ wss.on('connection', (ws) => {
         vehicleDataCount: vehicleDataCount
     }));
 
-    // Handle client disconnection
+    // handle client disconnection
     ws.on('close', () => {
         connectedClients--;
         console.log('Client disconnected. Total clients:', connectedClients);
     });
 });
 
-// Broadcast the vehicle data count to all clients every 10 seconds
+// broadcast the vehicle data count to all clients every 10 seconds
 setInterval(broadcastData, 10000);
 
-// Start HTTP and WebSocket server, but skip if it's running during tests
-if (process.env.NODE_ENV !== 'test') {
-    server.listen(port, async () => {
-        console.log("Server listening on port:", port);
-        await connectToDatabase();
-    });
-}
-
-module.exports = { app, server };
+// start HTTP and WebSocket server
+server.listen(port, async () => {
+    console.log("Server listening on port:", port);
+    await connectToDatabase();
+});
