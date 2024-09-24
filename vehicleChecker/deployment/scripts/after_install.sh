aab1810c7b9dea1ff510f0da45de0157432356df
Camille Ang <S223646635@deakin.edu.au>
2024-09-24 23:26:28 +1000

    Delete unnecessary files

--- vehicleChecker/deployment/scripts/after_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd  /Users/camilleangshujie/Documents/BILLIONAIRE/DEAKINU/TrimesterTwo/SIT753/vehicleChecker
-
-# Install dependencies
-npm install
-
-# Start the application 
-pm2 start src/server.js --name vehicleChecker 
